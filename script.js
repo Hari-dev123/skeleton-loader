5fa575c954edb5a4c1b7a27ee88cadfa6b5f892f
= <=>
2024-04-10 14:04:39 +0530

    js has added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hide the loader and show the card when the window has finished loading
+window.addEventListener('load', function() {
+
+    const card = document.querySelector('.card');
+
+
+    card.style.display = 'block';
+});
